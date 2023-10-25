c03f6cf398ba37c6ab8feab7364c423789ce7690
Apoxtrophe <cyggardner@gmail.com>
2023-10-24 20:40:16 -0600

    Removed Cursor for testing

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(PlayerPlugin)
         .add_systems(Startup, setup)
-        .add_systems(Update, setup_cursor)
-        .add_systems(Update, move_cursor)
         .run();
 }
 
@@ -39,42 +37,5 @@ fn setup(
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
     });
-}
-
-#[derive(Component)]
-struct GameCursor {}
-
-fn setup_cursor(
-    mut windows: Query<&mut Window>,
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
-    let mut window: Mut<Window> = windows.single_mut();
-    window.cursor.visible = false;
-    let cursor_spawn: Vec3 = Vec3::ZERO;
-
-    commands.spawn((
-        ImageBundle {
-            image: asset_server.load("assets/cursor.png").into(),
-            style: Style {
-                position_type: PositionType::Absolute,
-                left: Val::Auto,
-                bottom: Val::Auto,
-                ..default()
-            },
-            z_index: ZIndex::Global(15),
-            transform: Transform::from_translation(cursor_spawn),
-            ..default()
-        },
-        GameCursor {}
-    ));
-}
-
-fn move_cursor(window: Query<&Window>, mut cursor: Query<&mut Style, With<GameCursor>>) {
-    let window: &Window = window.single();
-    if let Some(position) = window.cursor_position() {
-        let mut img_style = cursor.single_mut();
-        img_style.left = Val::Px(position.x);
-        img_style.bottom = Val::Px(position.y);
-    }
+    
 }
